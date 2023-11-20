ac0b823e3b1c902a98a0f5b53af6dbe140ac6230
Aman Rawat <amannraawat@gmail.com>
2023-11-20 09:26:13 +0530

    make lastcut button for calculator

--- main.py
+++ main.py
@@ -34,6 +34,17 @@ def calculate():
         entry.insert(0, 'Error')
 
 
+def last_cut():
+    string = entry.get()
+    if len(string) != 0:
+        new_string = string[:-1]
+        all_clear()
+        entry.insert(0, new_string)
+    else:
+        all_clear()
+        entry.insert(0, '')
+
+
 entry = Entry(root)
 entry.grid(row=1, padx=4, pady=5, columnspan=8)
 
@@ -65,5 +76,8 @@ equalto_button.grid(row=5, column=4, padx=5, pady=5)
 
 point_button = Button(root, text='.', height=4, width=6, command=lambda: get_number('.'))
 point_button.grid(row=5, column=2, padx=5, pady=5)
+
+lastcut_button = Button(root, text='<--', height=4, width=6, command=last_cut)
+lastcut_button.grid(row=5, column=5, padx=5, pady=5)
 
 root.mainloop()
